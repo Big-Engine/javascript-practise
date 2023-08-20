e2b4cb63e3985e9c9048419d3d2b8a377d600051
[oluwadamilola ayodele] <[damsolmos@yahoo.com]>
2023-08-20 11:02:41 +0100

    first learning practice commit

---
+++ variables.js
@@ -0,0 +1,50 @@
+let damiAge = 50;
+let solaAge = 100;
+let sum = damiAge + solaAge;
+console.log(sum);
+
+let age = 15;
+console.log(age);
+// Updating age
+age = 100;
+console.log(age);
+
+// sampling the let variable
+let tax = 30;
+console.log(tax);
+
+// Update Tax
+tax = 50;
+console.log(tax);
+
+// using sum and other functions
+let amakaBill = 1000;
+let damiBill = 5000;
+let subtration = damiBill-amakaBill;
+let div = damiBill/amakaBill;
+console.log(amakaBill);
+console.log(damiBill);
+console.log(subtration);
+console.log(div);
+
+// Name and Surname
+let Name = "AYODELE";
+Name = "Ayodele Abisola";
+let surname = "Oluwadamilola.Big-Engine";
+
+console.log(Name);
+console.log(surname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
